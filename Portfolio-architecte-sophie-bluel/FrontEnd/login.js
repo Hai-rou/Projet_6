72f4108f32a3a15593b4c3f061be2a37c6581f56
Houmadi Hairou <hairou.houmadi@gmail.com>
2025-03-19 16:20:51 +0100

    add file

---
+++ Portfolio-architecte-sophie-bluel/FrontEnd/login.js
@@ -0,0 +1,68 @@
+const ApiUrl = "http://localhost:5678/api/"
+
+const myForm = document.getElementById('myForm');
+const baliseEmail = document.getElementById('email');
+const baliseMotdepass = document.getElementById('pass');
+const myError = document.getElementById('error');
+
+//Fonction de validation (1. check 2. login)
+
+function validationUser() {
+    const regExpmail = "[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+"
+    const regMdp = "^[A-Za-z0-9]{5,}$"
+    if(!regExpmail === baliseEmail.value) {
+        myError.innerHTML = "Le mail est incorrect"
+        myError.style.color = 'red'
+        return false
+    }
+    if(!regMdp === baliseMotdepass.value){
+        myError.innerHTML = "Le mot de passe est incorrect"
+        myError.style.color = 'red'
+        return false
+    }
+}
+
+myForm.addEventListener("submit", function(e) {
+    if(baliseMotdepass.value.trim() === ""){
+        myError.innerHTML = "Le mot de passe est incorrect"
+        myError.style.color = 'red'
+        e.preventDefault()
+    }
+    if(baliseEmail.value.trim() === ""){
+        myError.innerHTML = "Le mail est incorrect"
+        myError.style.color = 'red'
+        e.preventDefault()
+    }
+    validationUser(baliseEmail)
+    validationUser(baliseMotdepass)
+})
+
+
+//Login (fectch + recup token (Try/catch))
+
+myForm.addEventListener("submit", (event) => {
+    event.preventDefault()
+    let form = {baliseEmail,baliseMotdepass}
+    try {
+        fetch("http://localhost:5678/api/users/login/", {
+            method:'POST',
+            headers: {Accept : "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: form.baliseEmail.value,
+                password: form.baliseMotdepass.value,
+            }),
+        }).then((response) => {
+            response.json().then((data) => {
+                sessionStorage.setItem("token", data.token)
+                window.location.replace("index.html")
+                console.log(data)
+            })
+        })
+    }catch(erreur) {
+        console.error("Erreur 401 ou 404",erreur)
+    }
+})
+
+
---
+++ Portfolio-architecte-sophie-bluel/FrontEnd/script.js
@@ -0,0 +1,116 @@
+const ApiUrl = "http://localhost:5678/api/"
+let worksData;
+let categories;
+
+let filter;
+let gallery;
+
+/****Recupération des travaux depuis le back end***/
+window.onload = () =>{
+    fetch(`${ApiUrl}works`)
+    .then((response) => response.json())
+    .then((data) => {
+        worksData = data;
+        console.log(data)
+        /**Récupération des travaux***/
+        listOfcategories();
+        
+        dataGallery(worksData);
+        /***Filtre***/
+        filter = document.querySelector(".filter");
+        dataFilter(categories, filter)
+        /*****Get list of category***/
+        
+    })
+}
+
+/***Gallery****/
+function dataGallery(data) {
+    gallery = document.querySelector(".gallery");
+    gallery.innerHTML = "";
+     
+data.forEach((i) => {
+    
+    const workCard = document.createElement("figure");
+    const workImage = document.createElement("img");
+    const workTitle = document.createElement("figcaption");
+    workImage.src = i.imageUrl;
+    workImage.alt = i.title;
+    workTitle.innerText = i.title;
+    workCard.dataset.category = i.category.name;
+    workCard.className = "workCard";
+    
+    gallery.appendChild(workCard);
+    workCard.append(workImage, workTitle);
+  });
+}
+
+/****Filtre***/
+
+/****Get array category**/
+function listOfcategories() {
+    let listOfcategories = new Set();
+    worksData.forEach((work) => {
+        listOfcategories.add(JSON.stringify(work.category))
+    })
+
+    const arrayofStrings = [...listOfcategories];
+
+    categories = arrayofStrings.map((s) => JSON.parse(s));
+    console.log(categories)
+}
+//Init buttons
+function dataFilter(categories, filter){
+    const button =document.createElement("button");
+    button.innerText = "Tous";
+    button.className = "btn";
+    button.dataset.category = "Tous";
+    filter.appendChild(button);
+    filterBtn(categories, filter);
+    functionFilter();
+}
+
+
+/****Create button***/
+function filterBtn(categories, filter) {
+    categories.forEach((categorie) => {
+        createbtnfilter(categorie, filter)
+    })
+}
+
+function createbtnfilter(categorie, filter) {
+    const button = document.createElement("button");
+    button.innerText = categorie.name;
+    button.className = "btn"; 
+    button.dataset.category = categorie.name; 
+    filter.appendChild(button)
+}
+
+// Filter
+function functionFilter() {
+    const filterbuttons = document.querySelectorAll(".btn");
+    filterbuttons.forEach((i) => {
+        i.addEventListener("click", function () {
+            toggleProjects(i.dataset.category);
+        });
+    });
+}
+// 
+function toggleProjects(datasetCategory) {
+    const figures = document.querySelectorAll(".workCard");
+    if ("Tous" === datasetCategory) {
+        figures.forEach((figure) => {
+            figure.style.display = "block";
+        });
+    } else {
+        figures.forEach((figure) => {
+            figure.dataset.category === datasetCategory
+            ? (figure.style.display = "block")
+            : (figure.style.display = "none");
+        });
+    }
+}
+
+
+
+
